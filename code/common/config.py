482e42265fd62dabafcc429cd3ef8b85a22cdc1d
mac <mac.jablonski@outlook.com>
2025-08-07 16:10:40 -0400

    Add default webhook avatar URL to configuration

--- code/common/config.py
+++ code/common/config.py
@@ -16,6 +16,7 @@ class Config:
         )
         self._release_interval = 3600
         # ─── Server-side settings ─────────────────────────────────
+        self.DEFAULT_WEBHOOK_AVATAR_URL = "https://raw.githubusercontent.com/Copycord/Copycord/refs/heads/main/logo/logo.png"
         self.SERVER_TOKEN = os.getenv("SERVER_TOKEN")
         self.CLONE_GUILD_ID = os.getenv("CLONE_GUILD_ID", "0")
         self.DB_PATH = os.getenv("DB_PATH", "/data/data.db")
